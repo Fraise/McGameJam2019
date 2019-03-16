b3035ea6e92f3c5336692bbf7bc8c322bcbef7b5
bouergh <voodbutcher@gmail.com>
2019-03-15 22:48:22 -0400

    added menu and credits

---
+++ Assets/SceneChange.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChange : MonoBehaviour
+{
+
+    public void ChangeScene(string scene){
+        SceneManager.LoadScene(scene);
+    }
+}
