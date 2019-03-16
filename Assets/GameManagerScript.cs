12be4e6978765e32d63a48c4e327f2c95a756012
Marie Martin <marie.martin@polymtl.ca>
2019-03-16 13:41:11 +0100

    wip managers

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+    private DetectiveManager detectiveScript;
+
+    //Awake is always called before any Start functions
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+        detectiveScript = GetComponent<DetectiveManager>();
+        InitGame();
+    }
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+
+    }
+
+    //Update is called every frame.
+    void Update()
+    {
+
+    }
+}
