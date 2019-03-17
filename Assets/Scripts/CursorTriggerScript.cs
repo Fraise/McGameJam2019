b510bf1d7099a722873606ce458cb8df258242af
Marie Martin <marie.martin@polymtl.ca>
2019-03-17 09:34:19 -0400

    change cursor on mouse hover + fix objets button

---
+++ Assets/Scripts/CursorTriggerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorTriggerScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D cursor;
+    //Detect if the Cursor starts to pass over the GameObject
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    //Detect when Cursor leaves the GameObject
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
