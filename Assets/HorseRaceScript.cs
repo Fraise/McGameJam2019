dfed6fe408eb7476b2f4ff8c3e4d832332245a01
bouergh <voodbutcher@gmail.com>
2019-03-17 10:21:40 -0400

    outdoor chase + michuPNC

---
+++ Assets/HorseRaceScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class HorseRaceScript : MonoBehaviour
+{
+    //0 1 2 3
+    public int state = 0;
+    public VideoPlayer[] videoPlayers;
+    // Start is called before the first frame update
+    public void OnClick(){
+        state = (state+1) % 4;
+        videoPlayers[state].enabled = true;
+        videoPlayers[state].Play();
+    }
+}
