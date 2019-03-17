418d9f309df73f846e3cca183e494e15d395d764
bouergh <voodbutcher@gmail.com>
2019-03-16 23:48:33 -0400

    outdoor advance

---
+++ Assets/MultiVideoPlay.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class MultiVideoPlay : MonoBehaviour
+{
+    public VideoPlayer vp1, vp2;
+    public bool condition = false;
+    
+
+    public void OnClick(){
+        if(!condition){
+            vp1.Play();
+        }else{
+            vp2.Play();
+        }
+    }
+}
