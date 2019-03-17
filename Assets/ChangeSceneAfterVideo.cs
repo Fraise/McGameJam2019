5ca29831919d694197f9e1591665dbe0ece31586
bouergh <voodbutcher@gmail.com>
2019-03-16 23:20:11 -0400

    scene debut + setup outdoor

---
+++ Assets/ChangeSceneAfterVideo.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+public class ChangeSceneAfterVideo : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string scene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += PlayVideo;
+    }
+
+
+
+    void PlayVideo(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
