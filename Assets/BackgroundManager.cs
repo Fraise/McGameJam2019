c5c4452ff8e249ac08013c77b2456918661e5609
bouergh <voodbutcher@gmail.com>
2019-03-17 10:43:13 -0400

    race, outdoor

---
+++ Assets/BackgroundManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    public GameObject[] bgs;
+    public int state = 0; //1 with horse, 2 with mask
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
